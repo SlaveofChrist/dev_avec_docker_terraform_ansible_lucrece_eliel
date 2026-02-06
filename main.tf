f104649270d851317b0fc29c6a08aa2355d0de9d
Eliel WOTOBE <elielwotobe@gmail.com>
2026-02-06 20:44:17 +0100

    feat: Implement deployment workflow with Terraform and Ansible for fullstack application

--- main.tf
+++ main.tf
@@ -31,20 +31,21 @@ resource "tls_private_key" "pk" {
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name   = "registry-key-terraform"
+  key_name   = "app-key-terraform"
   public_key = tls_private_key.pk.public_key_openssh
 }
 
 resource "local_file" "ssh_key" {
-  filename        = "${path.module}/registry-key-terraform.pem"
+  filename        = "${path.module}/app-key-terraform.pem"
   content         = tls_private_key.pk.private_key_pem
   file_permission = "0400"
 }
 
-# 3. Security Group (Port 5000 ouvert !)
-resource "aws_security_group" "registry_sg" {
-  name        = "registry-sg-simple"
-  description = "Allow SSH, HTTP (UI), Registry (5000)"
+# 3. Security Group (Ports pour API, Frontend, DB, Redis)
+resource "aws_security_group" "app_sg" {
+  name        = "app-sg-fullstack"
+  description = "Allow SSH, Frontend (8080), API (8000), DB (5432), Redis (6379)"
+
   ingress {
     description = "SSH"
     from_port   = 22
@@ -52,20 +53,39 @@ resource "aws_security_group" "registry_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    description = "Registry UI"
-    from_port   = 80
-    to_port     = 80
+    description = "Frontend Nginx"
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    description = "Registry Docker API"
-    from_port   = 5000
-    to_port     = 5000
+    description = "API Python"
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Redis"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -75,23 +95,23 @@ resource "aws_security_group" "registry_sg" {
 }
 
 # 4. Instance EC2
-resource "aws_instance" "registry_server" {
+resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name      = aws_key_pair.generated_key.key_name
 
-  vpc_security_group_ids = [aws_security_group.registry_sg.id]
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
   tags = {
-    Name = "Terraform-Registry-Simple"
+    Name = "Terraform-App-FullStack"
   }
 }
 
 output "instance_ip" {
-  value = aws_instance.registry_server.public_ip
+  value = aws_instance.app_server.public_ip
 }
